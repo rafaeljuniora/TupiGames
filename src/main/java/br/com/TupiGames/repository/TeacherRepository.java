3db688a4c1a97f65c6aebcc447a7a817497bfc73
Rafael <160675357+rafaeljuniora@users.noreply.github.com>
2025-05-18 20:25:35 -0300

    fix: synced table data with db data

--- src/main/java/br/com/TupiGames/repository/TeacherRepository.java
+++ src/main/java/br/com/TupiGames/repository/TeacherRepository.java
@@ -1,10 +1,13 @@
 package br.com.TupiGames.repository;
 
+import br.com.TupiGames.domain.Escola;
 import br.com.TupiGames.domain.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Professor,Long> {
     Optional<Professor> findByEmail(String email);
+    List<Professor> findAllByescola (Escola escola);
 }
