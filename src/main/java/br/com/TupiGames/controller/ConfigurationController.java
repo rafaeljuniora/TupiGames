76dbdc9b91aa72b3bc40238648e44a229c9638cc
Davi <davidfeliciopsn@gmail.com>
2025-05-19 21:05:12 -0300

    fix: conflitos na branch resolvidos

---
+++ src/main/java/br/com/TupiGames/controller/ConfigurationController.java
@@ -0,0 +1,26 @@
+package br.com.TupiGames.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/configuration")
+public class ConfigurationController {
+
+    @GetMapping("/aluno")
+    public String loginSchoola() {
+        return "ConfigurationPages/StudentConfiguration";
+    }
+
+    @PostMapping("/escola/auth")
+    public String handleLogin(@RequestParam String email,
+                              @RequestParam String senha,
+                              Model model) {
+        // deu certo
+        return "redirect:/home";
+    }
+}
