fe53302d96e061fbd839e726ef7bc50e0180131e
Rafael <160675357+rafaeljuniora@users.noreply.github.com>
2025-05-28 20:04:46 -0300

    feat: new students home page

---
+++ src/main/java/br/com/TupiGames/controller/StudentController.java
@@ -0,0 +1,14 @@
+package br.com.TupiGames.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/aluno")
+public class StudentController {
+    @GetMapping("/home")
+    public String studentsHome(){
+        return "HomePages/StudentHome";
+    }
+}
