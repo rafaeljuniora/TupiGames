3f39e475bb722f871ac2b0cfc83729323f0e0633
Rafael <160675357+rafaeljuniora@users.noreply.github.com>
2025-06-05 16:02:10 -0300

    fix: making some class changes to integrate with ClassView

--- src/main/java/br/com/TupiGames/repository/ClassRepository.java
+++ src/main/java/br/com/TupiGames/repository/ClassRepository.java
@@ -1,14 +1,14 @@
 package br.com.TupiGames.repository;
 
 import br.com.TupiGames.domain.Escola;
+import br.com.TupiGames.domain.Professor;
 import br.com.TupiGames.domain.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ClassRepository extends JpaRepository <Turma, Long> {
     Turma findBynomeTurma(String nomeTurma);
     List<Turma> findAllByEscola(Escola escola);
+    List<Turma> findAllByProfessores(Professor professor);
 }
